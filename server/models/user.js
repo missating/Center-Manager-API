e7a92d9d9c291766b1e4bd366b3bc62f38cb3bc7
Vanessa Ating <atingenkay@gmail.com>
2018-03-20 10:09:03 +0100

    feature(user-signup): add codeclimate file add unique field to username in user model [Starts #156102778]

--- server/models/user.js
+++ server/models/user.js
@@ -16,6 +16,7 @@ export default (sequelize, DataTypes) => {
 
       username: {
         type: DataTypes.STRING,
+        unique: true,
         allownull: false,
         validate: {
           notEmpty: {
